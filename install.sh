78d115d999808a36de948e097d60cef720a8f5dc
minerdude <31804298+minerdude@users.noreply.github.com>
2018-01-06 22:14:11 -0300

    bug fixes

--- install.sh
+++ install.sh
@@ -9,12 +9,15 @@ sleep 3
 curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
 sudo apt update && sudo apt upgrade -y
 sudo apt install default-jre nodejs nano unzip jq curl
-npm install pm2@latest -g
+sudo npm install pm2@latest -g
 rm -f ProfitTrailer.zip
-wget "$(curl -s https://api.github.com/repos/taniman/profit-trailer/releases | jq -r '.[0].assets[].browser_download_url')" && \
-unzip ProfitTrailer.zip 
+rm -rf /temp
+wget -P ~/temp "$(curl -s https://api.github.com/repos/taniman/profit-trailer/releases | jq -r '.[0].assets[].browser_download_url')" && \
+unzip ProfitTrailer.zip
+rm -f ProfitTrailer.zip
+rm -rf ~/temp
 #Aliases
-if grep -q "#alias-pt" .bash_aliases; then
+if [ grep -q "#alias-pt" .bash_aliases ]; then
 	echo -e "\nAliases already created."
 else
 	echo -e "#alias-pt\nalias update='sudo apt update'\nalias upgrade='sudo apt upgrade'\nalias clean='sudo apt clean && sudo apt autoclean && sudo apt autoremove'\nalias upgrades='apt list --upgradable'\nalias pmls='pm2 ls'\nalias ptupd='rm -rf ~/temp/ && wget -P ~/temp/ "$(curl -s https://api.github.com/repos/taniman/profit-trailer/releases | jq -r '.[0].assets[].browser_download_url')" && unzip ~/temp/ProfitTrailer.zip -d temp/ && ptstop && mv ~/temp/ProfitTrailer/ProfitTrailer.jar ~/ProfitTrailer/ && cd ~'\nalias ptstart='cd ~/ProfitTrailer/ && pm2 start pm2-ProfitTrailer.json && cd ~'\nalias ptstop='pm2 stop profit-trailer'\nalias ptrestart='pm2 restart profit-trailer'/nalias ptdel='pm2 delete profit-trailer'/nalias='pm2 logs profit-trailer --lines 2000'" | tee -a ~/.bash_aliases
