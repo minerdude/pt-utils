382944059f091fdeb181219ce02366c1d23abdb5
minerdude <31804298+minerdude@users.noreply.github.com>
2018-02-16 21:07:40 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -47,10 +47,8 @@ unzip ProfitTrailer.zip > /dev/null 2>&1
 rm -f ProfitTrailer.zip
 ProgressBar 70 100
 #Aliases
-if [ -f "~/.bash_aliases" ] && [ grep -q "#alias-pt" ~/.bash_aliases ]
+if [ ! grep -Fxq "#alias-pt" ~/.bash_aliases ]
 then
-	$RED ; echo -e "\nAliases already created.\n" ; $RESET
-else
 	echo -e '#alias-pt\nalias update="sudo apt update"\nalias upgrade="sudo apt upgrade"\nalias clean="sudo apt clean && sudo apt autoclean && sudo apt autoremove"\nalias upgrades="apt list --upgradable"\nalias pmls="pm2 ls"\n'  | tee -a ~/.bash_aliases > /dev/null 2>&1
 	echo -e 'alias ptupd="cp -rf ~/ProfitTrailer/ ~/backupPT && rm -rf ~/temp/ && wget -P ~/temp/ $(curl -s https://api.github.com/repos/taniman/profit-trailer/releases | jq -r '.[0].assets[].browser_download_url') && unzip ~/temp/ProfitTrailer.zip -d temp/ && ptstop && mv ~/temp/ProfitTrailer/ProfitTrailer.jar ~/ProfitTrailer/ && cd ~"\nalias ptstart="cd ~/ProfitTrailer/ && pm2 start pm2-ProfitTrailer.json && cd ~"\nalias ptstop="pm2 stop profit-trailer"\nalias ptrestart="pm2 restart profit-trailer"\nalias ptdel="pm2 delete profit-trailer"\nalias ptlog="pm2 logs profit-trailer --lines 2000"' | tee -a ~/.bash_aliases > /dev/null 2>&1
 	source ~/.bash_aliases
@@ -62,9 +60,7 @@ sudo sed -i 's/Xmx512m/Xmx256m/' pm2-ProfitTrailer.json
 sudo sed -i 's/Xms512m/Xms256m/' pm2-ProfitTrailer.json
 ProgressBar 90 100
 #Sysctl Conf
-if [  -f "/etc/sysctl.d/99-profittrailer.conf" ]; then
-	$RED ; echo -e "\nSysconf already configured.\n" ; $RESET
-else
+if [ ! -f "/etc/sysctl.d/99-profittrailer.conf" ]; then
 	echo -e "\n#Protect Against TCP Time-Wait\nnet.ipv4.tcp_rfc1337 = 1\n" | sudo tee /etc/sysctl.d/99-profittrailer.conf > /dev/null 2>&1
 	echo -e "#Latency\nnet.ipv4.tcp_low_latency = 1\nnet.ipv4.tcp_slow_start_after_idle = 0\n" | sudo tee -a /etc/sysctl.d/99-profittrailer.conf > /dev/null 2>&1
 	echo -e "#Do less swapping\nvm.swappiness = 10\nvm.dirty_ratio = 10\nvm.dirty_background_ratio = 5\nvm.vfs_cache_pressure = 50\n" | sudo tee -a /etc/sysctl.d/99-profittrailer.conf > /dev/null 2>&1
